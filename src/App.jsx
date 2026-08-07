e41064b0001fdd7b9c9ab797b8fd7df3db3df8af
Defidecoder <defidecoder13@gmail.com>
2026-08-08 00:53:07 +0530

    fix: update X profile link to subham_sl

--- src/App.jsx
+++ src/App.jsx
@@ -426,7 +426,7 @@ export default function App() {
 
               {/* X / Twitter */}
               <a
-                href="https://x.com/defidecoder13"
+                href="https://x.com/subham_sl"
                 target="_blank"
                 rel="noreferrer"
                 className="hero-social-btn p-3 rounded-full bg-neutral-900 border border-neutral-800 text-neutral-400 hover:text-white hover:border-neutral-500 hover:bg-neutral-800/80 hover:shadow-[0_0_15px_rgba(255,255,255,0.25)] transition-[background-color,border-color,color,box-shadow,transform] duration-300 transform hover:-translate-y-1"
@@ -919,7 +919,7 @@ export default function App() {
                 <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24"><path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"/></svg>
               </a>
               <a 
-                href="https://x.com/defidecoder13" 
+                href="https://x.com/subham_sl" 
                 target="_blank" 
                 rel="noreferrer"
                 className="hover:text-white transition-colors duration-300"
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -121,7 +121,7 @@ export default function Sidebar({ onOpenContact, onOpenBadge, activeSection }) {
             <a href="https://linkedin.com/in/defidecoder13" target="_blank" rel="noreferrer" className="text-slate-400 hover:text-white transition-colors p-2 rounded-lg hover:bg-slate-800/50" title="LinkedIn">
               <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24"><path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.28 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.75M6.46 10.9v8.37H9.25V10.9H6.46M7.86 6.77a1.62 1.62 0 1 0 0 3.24 1.62 1.62 0 0 0 0-3.24z"/></svg>
             </a>
-            <a href="https://x.com/defidecoder13" target="_blank" rel="noreferrer" className="text-slate-400 hover:text-white transition-colors p-2 rounded-lg hover:bg-slate-800/50" title="X / Twitter">
+            <a href="https://x.com/subham_sl" target="_blank" rel="noreferrer" className="text-slate-400 hover:text-white transition-colors p-2 rounded-lg hover:bg-slate-800/50" title="X / Twitter">
               <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
             </a>
           </div>
